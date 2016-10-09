7c4ab3e0a3a5d46031132dc1af1ee50afc4ba43a
starkhorn <t.earendel@gmail.com>
2016-10-09 21:56:38 +0700

    Implement mock kanban lanes.

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,38 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-  </div>
+<div id="app">
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="header">
+          <h1 class="title is-1">Vue Kanban</h1>
+        </div>
+
+        <div class="tile is-ancestor">
+          <Lane title="TODO" />
+          <Lane title="In Progress" />
+          <Lane title="Done" />
+        </div>
+      </div>
+
+    </div>
+  </section>
+</div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import Lane from './components/Lane';
 
 export default {
   components: {
-    Hello,
-  },
+    Lane
+  }
 };
 </script>
 
-<style>
-html {
-  height: 100%;
-}
+<style lang="scss">@import 'node_modules/bulma/bulma';</style>
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
+<style scope>
+.header {
+  margin-bottom: 20px;
 }
 </style>
---
+++ src/components/Lane.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="tile is-4 is-parent">
+    <div class="tile is-child box lane">
+      <div class="title is-5">
+        {{title}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scope>
+.lane {
+  height: 70vh;
+}
+</style>
